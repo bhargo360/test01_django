619d95aba8ec2638d75624b3e4329ea6ef364216
Bhargav Bhatt <bhargo360@gmail.com>
2023-07-17 00:04:23 +0200

    adding database

--- web01/app01/models.py
+++ web01/app01/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class App01(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField()
+  birth_date = models.DateField()
--- web01/app01/views.py
+++ web01/app01/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import App01
 
 def app01(request):
-    template = loader.get_template('template01.html')
-    return HttpResponse(template.render())
+    mymembers = App01.objects.all().values()
+    template = loader.get_template('template02.html')
+    context = {
+    'mymembers': mymembers,
+  }
+    return HttpResponse(template.render(context, request))
